18b53ffe135e6f3d6fa265e8e2440a39cbedbc19
周公来 <zhougonglai@nn.com>
2021-05-09 23:04:06 +0800

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import sveltePreprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess({ postcss: true }),
 	kit: {
+		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
